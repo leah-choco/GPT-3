a3912089118a18d82eaa1109b8a380bd9dfabad4
Your Name <you@example.com>
2023-02-15 14:09:49 -0600

    Adding links for the Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+//import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 
 import logo from '../../assets/logo.svg';
@@ -10,9 +10,17 @@ const Navbar = () => {
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-          <img src={logo} alt-='logo'/>
+          <img src={logo} alt='logo'/>
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT3?</a></p>
+          <p><a href='#possibility'>OpenAI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
 
         </div>
+
 
       </div>
     </div>
