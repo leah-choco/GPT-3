592b8da1728dbf6afe5eb2be278eedde255d25b2
Your Name <you@example.com>
2023-02-15 15:15:20 -0600

    Adding code for useState to use toggleMenu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
-//import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
 import './navbar.css';
 
 import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
+  const[toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -26,8 +28,29 @@ const Navbar = () => {
         <p>Sign In</p>
         <button type='button'>Sign Up</button>
       </div>
+      
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu 
+          ? <RiCloseLine  color='#fff' size={27} onClick={() => setToggleMenu(false)}/> 
+          : <RiMenu3Line  color='#fff' size={27} onClick={() => setToggleMenu(true)}/> }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu-container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <p><a href='#home'>Home</a></p>
+              <p><a href='#wgpt3'>What is GPT?</a></p>
+              <p><a href='#possibility'>OpenAI</a></p>
+              <p><a href='#features'>Case Studies</a></p>
+              <p><a href='#blog'>Library</a></p>
+            </div>
+            <div className='gpt3__navbar-menu_container-links-sign'>
+              <p>Sign In</p>
+              <button type='button'>Sign Up</button>
+            </div>
+          </div>
+        )}  
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
