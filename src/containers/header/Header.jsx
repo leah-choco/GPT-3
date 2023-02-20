ed5dd9e4169e95d0af8135d463811b7581a46708
Your Name <you@example.com>
2023-02-20 14:58:35 -0600

    Adding p tag?

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>Let's Build Something Amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <div className='gpt3__header-content_input'>
           <input type='email' placeholder='Your email address'></input>
           <button type='button'>Get Started</button>
