b0f83fe0d9e3a5a7c41f7dcb6e9ee1b5c31dc9b7
Your Name <you@example.com>
2023-02-20 14:55:45 -0600

    Adding input and button

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,6 +7,10 @@ const Header = () => {
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>Let's Build Something Amazing with GPT-3 OpenAI</h1>
+        <div className='gpt3__header-content_input'>
+          <input type='email' placeholder='Your email address'></input>
+          <button type='button'>Get Started</button>
+        </div>
       </div>
       
     </div>
