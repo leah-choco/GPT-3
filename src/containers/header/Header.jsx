5278cae27f348042a4ab8f4fc6552db271aa97a9
Your Name <you@example.com>
2023-02-20 14:51:17 -0600

    Getting inital classes set up for the header. h1 appearing now with gradient text

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,12 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's Build Something Amazing with GPT-3 OpenAI</h1>
+      </div>
+      
+    </div>
   )
 }
 
