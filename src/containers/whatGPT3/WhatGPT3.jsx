68d29e8b063d03073e29089a5b329dc404a38020
Your Name <you@example.com>
2023-02-23 13:04:08 -0600

    Adding another feature container

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -10,12 +10,17 @@ const WhatGPT3 = () => {
       <div className='gpt3__whatgpt3-feature'>
         <Feature />
       </div>
+
       <div className='gpt3__whatgpt3-heading'>
         <h1 className='gradient-text'> The possibilites are beyond your imagination</h1>
         <p>Explore the Library</p>
-
       </div>
 
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
