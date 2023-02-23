fe13e8e6fd0bd307c802d2fd4568698000b95b7c
Your Name <you@example.com>
2023-02-23 13:39:58 -0600

    Adding text, need to add props

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -9,6 +9,10 @@ const Features = () => {
         <div />
         <h1>{title}</h1>
       </div>
+
+      <div className='gpt3__features-container_feature-text'>
+        {text}
+      </div>
     </div>
   )
 }
