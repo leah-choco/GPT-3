a68fa7e05bd0b7c43eea446c6251073cbb2d4590
Your Name <you@example.com>
2023-02-28 15:22:02 -0600

    Adding divs to render date and text

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -7,6 +7,12 @@ const Article = ({ imgUrl, date, text }) => (
     <div className="gpt3__blog-container_article-image">
       <img src={imgUrl} alt="blog_image" />
     </div>
+    <div className="gpt3__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      </div>
   </div>
   )
 
