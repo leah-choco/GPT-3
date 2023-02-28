9c3e11715da72f9ffb60020b659a2059c8b7eeab
Your Name <you@example.com>
2023-02-28 15:20:39 -0600

    Initial jsx for article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './article.css';
 
-const Article = () => {
-  return (
-    <div>Article</div>
+const Article = ({ imgUrl, date, text }) => (
+  <div className="gpt3__blog-container_article">
+    <div className="gpt3__blog-container_article-image">
+      <img src={imgUrl} alt="blog_image" />
+    </div>
+  </div>
   )
-}
+
 
 export default Article
