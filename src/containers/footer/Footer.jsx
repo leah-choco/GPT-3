82c969a40f500a0091b898977eb78c3211aad79d
Your Name <you@example.com>
2023-03-02 14:25:56 -0600

    Adding heading for the footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
 
 import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      </div>
+    </div>
   )
 }
 
